67b1737f5b2310f220f6b5536fe9cb76893e009d
jose <jose_zuniga93@yahoo.com>
2020-12-04 15:44:37 -0600

    added user routes and actions note_to_self havent finished testing everything

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,52 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  # def new
+  #   render "new.html"
+  # end
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      Street_address: params[:Street_address],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      #password: params[:password], need to add password in schema
+
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    @users = User.all
+    render "show.html.erb"
+  end
+
+  # def update
+  #   @users = current_user
+  #     first_name: params[:first_name] || @user.first_name,
+  #     last_name: params[:last_name]|| @user.last_name,
+  #     street_address: params[:street_address]|| @user.street_address,
+  #     email: params[:email]|| @user.email,
+  #     phone_number: params[:phone_number]|| @user.phone_number,
+  #     password: params[:password]|| @user.password,
+  #   @users.save
+  #   render "show.json.jb"
+  # end
+  #*****************dec 4 2020 today i ended by cheking if my update function is working   so far bearly started to look into it
+
+  def destroy
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    @user.destroy
+    render json: { message: "User is now deleted!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+    #get "/users" => "users#new"
+    post "/users" => "users#create"
+    #get "/user/:id" => "user#show"
+    #get "/user/:id/:edit" => "/user#edit"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+  end
 end
